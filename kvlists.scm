cc8e09e2ae64dabfbdfd6cf924ad22bede1ff5a2
Peter Danenberg <pcd@roxygen.org>
2012-07-29 21:56:58 -0700

    Port to Chicken 4.

--- kvlists.scm
+++ kvlists.scm
@@ -20,129 +20,127 @@
 ;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 ;; IN THE SOFTWARE.
 
-(require-extension srfi-1)
+(module kvlists
+  (keyword->symbol
+   symbol->keyword
+   alist?
+   alist->kvlist
+   alist->plist
+   plist?
+   plist->kvlist
+   plist->alist
+   kvlist?
+   kvlist-key?
+   kvlist-ref
+   kvlist-set!
+   kvlist-map
+   kvlist-for-each
+   kvlist-assoc
+   kvlist-cons*
+   kvlist-delete
+   kvlist-delete!
+   kvlist->plist
+   kvlist->alist)
+  (import chicken
+          data-structures
+          scheme)
+  (use srfi-1)
 
-#+chicken
-(eval-when (compile)
-  (declare
-    (export keyword->symbol
-            symbol->keyword
-            alist?
-            alist->kvlist
-            alist->plist
-            plist?
-            plist->kvlist
-            plist->alist
-            kvlist?
-            kvlist-key?
-            kvlist-ref
-            kvlist-set!
-            kvlist-map
-            kvlist-for-each
-            kvlist-assoc
-            kvlist-cons*
-            kvlist-delete
-            kvlist-delete!
-            kvlist->plist
-            kvlist->alist)))
+  ;; Keyword <-> symbol conversions (not provided by Chicken, at the moment)
 
-;;;; Exported procedures
+  (define (symbol->keyword symbol)
+    (string->keyword (symbol->string symbol)))
 
-;;; Keyword <-> symbol conversions (not provided by Chicken, at the moment)
+  (define (keyword->symbol keyword)
+    (string->symbol (keyword->string keyword)))
 
-(define (symbol->keyword symbol)
-  (string->keyword (symbol->string symbol)))
+  ;; Key-value list predicates
 
-(define (keyword->symbol keyword)
-  (string->symbol (keyword->string keyword)))
+  (define (alist? x)
+    (and (proper-list? x)
+         (every pair? x)))
 
-;;; Key-value list predicates
+  (define (plist? x)
+    (and (proper-list? x)
+         (= (modulo (length x) 2) 0)))
 
-(define (alist? x)
-  (and (proper-list? x)
-       (every pair? x)))
+  (define (kvlist? x)
+    (and (proper-list? x)
+         (= (modulo (length x) 2) 0)
+         (every (lambda (kv) (keyword? (car kv)))
+                (chop x 2))))
 
-(define (plist? x)
-  (and (proper-list? x)
-       (= (modulo (length x) 2) 0)))
+  ;; Key-value list accessors and operations (as provided by SRFI-1 for alists)
+  ;; (Note that these procedures currently assume that keywords occur only as
+  ;; keys, never as values. This should probably be corrected.)
 
-(define (kvlist? x)
-  (and (proper-list? x)
-       (= (modulo (length x) 2) 0)
-       (every (lambda (kv) (keyword? (car kv)))
-              (chop x 2))))
+  (define (kvlist-key? kvlist key)
+    (not (not (memq key kvlist))))
 
-;;; Key-value list accessors and operations (as provided by SRFI-1 for alists)
-;;; (Note that these procedures currently assume that keywords occur only as
-;;; keys, never as values. This should probably be corrected.)
+  (define (kvlist-ref kvlist key #!optional default)
+    (cond ((memq key kvlist) => cadr)
+          (else default)))
 
-(define (kvlist-key? kvlist key)
-  (not (not (memq key kvlist))))
+  (define (kvlist-set! kvlist key value)
+    (cond ((memq key kvlist) => (lambda (kv*) (set-car! (cdr kv*) value)))
+          (else (append! kvlist (list (if (keyword? key) key
+                                          (symbol->keyword key))
+                                      value)))))
 
-(define (kvlist-ref kvlist key #!optional default)
-  (cond ((memq key kvlist) => cadr)
-        (else default)))
+  (define (kvlist-map proc kvlist)
+    (map! (lambda (kv)
+            (proc (car kv) (cadr kv)))
+          (chop kvlist 2)))
 
-(define (kvlist-set! kvlist key value)
-  (cond ((memq key kvlist) => (lambda (kv*) (set-car! (cdr kv*) value)))
-        (else (append! kvlist (list (if (keyword? key) key
-                                        (symbol->keyword key))
-                                    value)))))
+  (define (kvlist-for-each proc kvlist)
+    (for-each (lambda (kv)
+                (proc (car kv) (cadr kv)))
+      (chop kvlist 2)))
 
-(define (kvlist-map proc kvlist)
-  (map! (lambda (kv)
-          (proc (car kv) (cadr kv)))
-        (chop kvlist 2)))
+  (define (kvlist-assoc key kvlist)
+    (cond ((memq key kvlist) => (lambda (kv*) (cons (car kv*) (cadr kv*))))
+          (else #f)))
 
-(define (kvlist-for-each proc kvlist)
-  (for-each (lambda (kv)
-              (proc (car kv) (cadr kv)))
-            (chop kvlist 2)))
+  (define (kvlist-cons* key value kvlist)
+    (cons* key value kvlist))
 
-(define (kvlist-assoc key kvlist)
-  (cond ((memq key kvlist) => (lambda (kv*) (cons (car kv*) (cadr kv*))))
-        (else #f)))
+  (define (kvlist-delete key kvlist)
+    (concatenate! (remove! (lambda (kv) (eq? (car kv) key))
+                           (chop kvlist 2))))
 
-(define (kvlist-cons* key value kvlist)
-  (cons* key value kvlist))
+  (define kvlist-delete! kvlist-delete)
 
-(define (kvlist-delete key kvlist)
-  (concatenate! (remove! (lambda (kv) (eq? (car kv) key))
-                         (chop kvlist 2))))
+  ;; Key-value list <-> association list conversions
 
-(define kvlist-delete! kvlist-delete)
+  (define (kvlist->alist kvlist)
+    (map! (lambda (kv)
+            (cons (keyword->symbol (car kv)) (cadr kv)))
+          (chop kvlist 2)))
 
-;;; Key-value list <-> association list conversions
+  (define (alist->kvlist alist)
+    (append-map! (lambda (kv)
+                   (list (symbol->keyword (car kv)) (cdr kv)))
+                 alist))
 
-(define (kvlist->alist kvlist)
-  (map! (lambda (kv)
-          (cons (keyword->symbol (car kv)) (cadr kv)))
-        (chop kvlist 2)))
+  ;; Key-value list <-> property list conversions
 
-(define (alist->kvlist alist)
-  (append-map! (lambda (kv)
-                 (list (symbol->keyword (car kv)) (cdr kv)))
-               alist))
+  (define-inline (kv-mutator car-mutator)
+    (lambda (kv)
+      (set-car! kv (car-mutator (car kv))) kv))
 
-;;; Key-value list <-> property list conversions
+  (define (plist->kvlist plist)
+    (append-map! (kv-mutator symbol->keyword)
+                 (chop plist 2)))
 
-(define-inline (kv-mutator car-mutator)
-  (lambda (kv)
-    (set-car! kv (car-mutator (car kv))) kv))
+  (define (kvlist->plist kvlist)
+    (append-map! (kv-mutator keyword->symbol)
+                 (chop kvlist 2)))
 
-(define (plist->kvlist plist)
-  (append-map! (kv-mutator symbol->keyword)
-               (chop plist 2)))
+  ;; Property list <-> association list conversions
+  ;; (Simple & naive implementation as these are provided as a bonus only.)
 
-(define (kvlist->plist kvlist)
-  (append-map! (kv-mutator keyword->symbol)
-               (chop kvlist 2)))
+  (define (plist->alist plist)
+    (kvlist->alist (plist->kvlist plist)))
 
-;;; Property list <-> association list conversions
-;;; (Simple & naive implementation as these are provided as a bonus only.)
-
-(define (plist->alist plist)
-  (kvlist->alist (plist->kvlist plist)))
-
-(define (alist->plist alist)
-  (kvlist->plist (alist->kvlist alist)))
+  (define (alist->plist alist)
+    (kvlist->plist (alist->kvlist alist))))
